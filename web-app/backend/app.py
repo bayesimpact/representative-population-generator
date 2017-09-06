a23197e04abdec37d16dc9b2f1881b80ef1114b1
Eric Boucher <ericboucher@bayesimpact.org>
2017-09-06 10:37:08 -0700

    Move to web-app folder

--- web-app/backend/app.py
+++ web-app/backend/app.py
@@ -19,6 +19,7 @@ def get_zip_county_points(zipcode, county):
     number = str(random.randint(1, 100))  # TODO: replace this with model
     return number
 
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.config['DEBUG'] = True
