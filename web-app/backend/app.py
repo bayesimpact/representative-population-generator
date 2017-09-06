61c292ad27eb909c4ac08755d53bae2c899a9230
Mehdi <m.jameie@gmail.com>
2017-09-06 16:28:39 -0700

    linting

--- web-app/backend/app.py
+++ web-app/backend/app.py
@@ -91,6 +91,7 @@ def no_op_handler():
     """Return 200 to any OPTIONS request."""
     return None
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.config['DEBUG'] = True
