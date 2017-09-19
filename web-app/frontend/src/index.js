31fe712fa9172b9a9fb1a95c1dad6ecba92b236e
Stephan Gabler <stephan.gabler@gmail.com>
2017-09-19 09:58:17 +0200

    Initialize a new frontend app
    And copy the old one to a backup folder so I still have access to it for inspiration. I wanted to start from scratch because some of the reducers were a bit hard to understand.

--- web-app/frontend/src/index.js
+++ web-app/frontend/src/index.js
@@ -1,25 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-import reducer from './reducers'
-import App from './containers/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const middleware = [ thunk ]
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger())
-}
-
-const store = createStore(
-  reducer,
-  applyMiddleware(...middleware)
-)
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
