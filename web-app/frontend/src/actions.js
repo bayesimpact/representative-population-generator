da53242e8c382606cfa8373aabcf582d537859d3
Stephan Gabler <stephan.gabler@gmail.com>
2017-09-21 10:43:53 +0200

    Refactor actions for readability

--- web-app/frontend/src/actions.js
+++ web-app/frontend/src/actions.js
@@ -1,91 +1,66 @@
 import * as api from './api'
 
-export const FETCH_COUNTIES = 'FETCH_COUNTIES'
 export const START_REQUEST = 'START_REQUEST'
 export const FINISH_REQUEST = 'FINISH_REQUEST'
 export const SET_APP_VARIABLE = 'SET_APP_VARIABLE'
 
 
 export const fetchCounties = () => dispatch => {
-  dispatch({type: START_REQUEST, resource: 'counties'})
+  dispatch(startRequestAction('counties'))
   api.getCounties().then(counties => {
-    dispatch({
-      type: FINISH_REQUEST,
-      resource: 'counties',
-      result: counties,
-    })
+    dispatch(finishRequestAction('counties', counties))
   })
 }
 
 export const fetchAreas = selectedCountyZips => dispatch => {
-  dispatch({
-    type: SET_APP_VARIABLE,
-    value: '',
-    variable: 'selectedCSVFileName',
-  })
-  dispatch({type: START_REQUEST, resource: 'areas'})
+  dispatch(setAppVariableAction('selectedCSVFileName', ''))
+  dispatch(startRequestAction('areas'))
   const countyZips = selectedCountyZips.map(countyZip => {
     const [county, zip] = countyZip.split('-')
     return {county, zip}
   })
   api.getAreas(countyZips).then(areas => {
-    dispatch({
-      type: FINISH_REQUEST,
-      resource: 'areas',
-      result: areas,
-    })
+    dispatch(finishRequestAction('areas', areas))
   })
 }
 
 export const fetchAreasFromCSVFile = file => dispatch => {
-  dispatch({
-    type: SET_APP_VARIABLE,
-    value: file.name,
-    variable: 'selectedCSVFileName',
-  })
-  dispatch({type: START_REQUEST, resource: 'areas'})
+  dispatch(setAppVariableAction('selectedCSVFileName', file.name))
+  dispatch(startRequestAction('areas'))
   api.getAreasFromFile(file).then(areas => {
-    dispatch({
-      type: FINISH_REQUEST,
-      resource: 'areas',
-      result: areas,
-    })
+    dispatch(finishRequestAction('areas', areas))
   })
 }
 
 export function setSelectedCounties(selectedCounties) {
-  return {
-    type: SET_APP_VARIABLE,
-    value: selectedCounties,
-    variable: 'selectedCounties',
-  }
+  return setAppVariableAction('selectedCounties', selectedCounties)
 }
 
 export function setSelectedCountyZips(selectedCountyZips) {
-  return {
-    type: SET_APP_VARIABLE,
-    value: selectedCountyZips,
-    variable: 'selectedCountyZips',
-  }
+  return setAppVariableAction('selectedCountyZips', selectedCountyZips)
 }
 
 export function setViewMode(viewMode) {
-  return {
-    type: SET_APP_VARIABLE,
-    value: viewMode,
-    variable: 'viewMode',
-  }
+  return setAppVariableAction('viewMode', viewMode)
 }
 
 export function setPointNumber(nPoints) {
-  return {
-    type: SET_APP_VARIABLE,
-    value: nPoints,
-    variable: 'nPoints',
-  }
+  return setAppVariableAction('nPoints', nPoints)
 }
 
 export const resetAreaSelector = () => dispatch => {
   dispatch(setSelectedCounties([]))
   dispatch(setSelectedCountyZips([]))
+}
+
+function setAppVariableAction(variable, value) {
+  return {type: SET_APP_VARIABLE, value, variable}
+}
+
+function startRequestAction(resource) {
+  return {type: START_REQUEST, resource}
+}
+
+function finishRequestAction(resource, result) {
+  return {type: FINISH_REQUEST, resource, result}
 }
