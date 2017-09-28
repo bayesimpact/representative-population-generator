467df60e2ab09336f624fb7c3d261c70679e1e5a
Eric Boucher <ericboucher@bayesimpact.org>
2017-09-28 15:02:28 -0700

    Add tagging

--- deploy/build_prod_docker.sh
+++ deploy/build_prod_docker.sh
@@ -14,9 +14,19 @@ then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # Handle exits from shell or function but don't exit interactive shell.
 fi
 
+if [ "$1" == "latest" ]; then
+  readonly TAG="$1"
+else
+  readonly TAG=$(git describe --always)
+  echo "Building with tag ${TAG}."
+fi
+
+export TAG="${TAG}"
+
 echo "Building and pushing database Docker."
 docker build \
     -f data/Dockerfile.prod \
+    -t bayesimpact/$DB_DOCKER_IMAGE:$TAG \
     -t bayesimpact/$DB_DOCKER_IMAGE:latest \
     ./data
 docker push bayesimpact/$DB_DOCKER_IMAGE
@@ -24,6 +34,7 @@ docker push bayesimpact/$DB_DOCKER_IMAGE
 echo "Building and pushing backend Docker."
 docker build \
     -f web-app/backend/Dockerfile.prod \
+    -t bayesimpact/$BACKEND_DOCKER_IMAGE:$TAG \
     -t bayesimpact/$BACKEND_DOCKER_IMAGE:latest \
     ./web-app/backend
 docker push bayesimpact/$BACKEND_DOCKER_IMAGE
@@ -31,13 +42,14 @@ docker push bayesimpact/$BACKEND_DOCKER_IMAGE
 echo "Building and pushing frontend Docker."
 docker build \
     -f web-app/frontend/Dockerfile.prod \
+    -t bayesimpact/$FRONTEND_DOCKER_IMAGE:$TAG \
     -t bayesimpact/$FRONTEND_DOCKER_IMAGE:latest \
     ./web-app/frontend
 docker push bayesimpact/$FRONTEND_DOCKER_IMAGE
 
 
 # Deploy to ECS.
-read -p "Would you like to deploy these new images? Are you sure? " -n 1 -r
+read -p "Would you like to deploy these new images with tag ${TAG}? Are you sure? " -n 1 -r
 echo    # Move to a new line.
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
--- deploy/initialize_cluster.sh
+++ deploy/initialize_cluster.sh
@@ -10,6 +10,10 @@ then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # Handle exits from shell or function but don't exit interactive shell.
 fi
 
-ecs-cli up --keypair na-server --capability-iam --size 2 --instance-type m3.medium --port 80 --port 8080 --port 443
+ecs-cli up --keypair na-server --capability-iam --size 2 --instance-type m4.xlarge --port 80 --port 8080 --port 443
+
+# Export TAG to use in docker-compose.
+# Later deploys using build_prod_docker use git commit tags.
+export TAG="latest"
 ecs-cli compose --file deploy/docker-compose-prod.yml --cluster network-adequacy service create --load-balancer-name network-adequacy-lb
 ecs-cli compose --file deploy/docker-compose-prod.yml --cluster network-adequacy scale 2
