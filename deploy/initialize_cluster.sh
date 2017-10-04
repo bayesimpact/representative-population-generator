7fb0de3955745fbdf012517fa2ad733628840757
Eric Boucher <eric.p.boucher@gmail.com>
2017-10-04 10:37:17 -0700

    Use m4.large

--- deploy/initialize_cluster.sh
+++ deploy/initialize_cluster.sh
@@ -10,10 +10,14 @@ then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # Handle exits from shell or function but don't exit interactive shell.
 fi
 
-ecs-cli up --keypair na-server --capability-iam --size 2 --instance-type m4.xlarge --port 80 --port 8080 --port 443
+N_INSTANCES=1
+
+ecs-cli up --keypair na-server --capability-iam --size $N_INSTANCES --instance-type m4.large --port 80 --port 8080 --port 443
 
 # Export TAG to use in docker-compose.
 # Later deploys using build_prod_docker use git commit tags.
 export TAG="latest"
 ecs-cli compose --file deploy/docker-compose-prod.yml --cluster network-adequacy service create --load-balancer-name network-adequacy-lb
-ecs-cli compose --file deploy/docker-compose-prod.yml --cluster network-adequacy scale 2
+
+# Scale the number of tasks with the number of instances.
+ecs-cli compose --file deploy/docker-compose-prod.yml --cluster network-adequacy scale $N_INSTANCES
